a9318ddc6ed472c6ad6100216cc2b1129c2b7c49
unknown <mshivam76@gmail.com>
2020-12-30 20:58:56 +0530

    Handling secrets with Environment Variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagrammishradev",
+    "password": "udagrammishradev",
+    "database": "udagrammishradev",
+    "host": "udagrammishradev.cm20shuhfzkz.ap-south-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "ap-south-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagram-mishra-dev"
   },
   "prod": {
     "username": "",
--- src/controllers/v0/feed/routes/feed.router.ts
+++ src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,51 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
-
+router.get('/:id', 
+    requireAuth, 
+    async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const items = await  FeedItem.findOne({
+        where: {
+          id: id,
+        },
+        raw: false,
+      });
+    //   .then((result) => {
+    //     res.send(result);
+    //   });
+    // items.rows.map((item) => {
+    //         if(item.url) {
+    //             item.url = AWS.getGetSignedUrl(item.url);
+    //         }
+    // });
+    res.send(items);
+});
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        const {id} = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+           // check Caption is valid
+        const item = new FeedItem({
+            caption: caption,
+            url: fileName
+        });
+        if (!caption) {
+            return res.status(400).send({ message: 'Caption is required or malformed' });
+        }
+
+        // check Filename is valid
+        if (!fileName) {
+            return res.status(400).send({ message: 'File url is required' });
+        }
+        const saved_item = await item.save();
+
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
 });
 
 
