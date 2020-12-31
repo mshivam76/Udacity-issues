7eb5e39281a41f458b1ad27f26c3f86fb60730a7
Shivam <mshivam76@gmail.com>
2020-12-31 16:11:34 +0530

    Config file changes-security updates

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,30 +1,13 @@
 export const config = {
-  // "dev": {
-  //   "username": process.env.POSTGRES_USERNAME,
-  //   "password": process.env.POSTGRES_USERNAME,
-  //   "database": process.env.POSTGRES_USERNAME,
-  //   "host": process.env.POSTGRES_HOST,
-  //   "dialect": "postgres",
-  //   "aws_region": process.env.AWS_REGION,
-  //   "aws_profile": process.env.AWS_PROFILE,
-  //   "aws_media_bucket": process.env.S3_MEDIA_BUCKET.
-  // },
-  // "prod": {
-  //   "username": "",
-  //   "password": "",
-  //   "database": "udagram_prod",
-  //   "host": "",
-  //   "dialect": "postgres"
-  // }
   "dev": {
-    "username": "udagrammishradev",
-    "password": "udagrammishradev",
-    "database": "udagrammishradev",
-    "host": "udagrammishradev.cm20shuhfzkz.ap-south-1.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "ap-south-1",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-mishra-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.S3_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
